4341c923088398de2a66ce783978890d8cd95f45
Bradley Guthrie <guppy96@gmail.com>
2016-07-11 20:07:30 -0500

    change case of username and notes

--- Program.cs
+++ Program.cs
@@ -11,7 +11,15 @@ namespace Cheers
         {
             Console.WriteLine("What is your name?");
             Console.Write("> ");
-            string user_name = Console.ReadLine();
+            string user_name = Console.ReadLine(); //Read from terminal, save to variable
+            
+            // user_name.ToUpper() is turning the user name to upper case.
+            // ToUpper() is functionality specific to Strings
+            // ToUpper() accepts not argumnts            
+            
+            Console.WriteLine(user_name.ToUpper() + " is.. GRAND!");  
+
+                      
             
         }
     }
