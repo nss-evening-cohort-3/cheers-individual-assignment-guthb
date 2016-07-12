2d47b87dfac9ae197f5d36f6cc4391860f80c3fe
Bradley Guthrie <guppy96@gmail.com>
2016-07-11 20:21:49 -0500

    added loop for printing out each letter of the name

--- Program.cs
+++ Program.cs
@@ -13,10 +13,20 @@ namespace Cheers
             Console.Write("> ");
             string user_name = Console.ReadLine(); //Read from terminal, save to variable
             
+            for (int i = 0; i < user_name.Length; i++)
+            {
+                Console.WriteLine("give me a.. " + user_name[i]); 
+            }
+
+
             // user_name.ToUpper() is turning the user name to upper case.
             // ToUpper() is functionality specific to Strings
             // ToUpper() accepts not argumnts            
             
+            // loop through the user_name array
+
+        
+
             Console.WriteLine(user_name.ToUpper() + " is.. GRAND!");  
 
                       
